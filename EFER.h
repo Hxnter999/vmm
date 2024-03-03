98f59cdd65d1c6fab2464ceb32de78b7f42268ea
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-03-04 02:00:29 +0300

    snake

--- EFER.h
+++ EFER.h
@@ -7,23 +7,23 @@ struct EFER
 
 	union {
 		struct {
-			uint64_t SCE : 1; // System Call Extensions
+			uint64_t sce : 1; // System Call Extensions
 			uint64_t reserved1 : 7;
-			uint64_t LME : 1; // Long Mode Enable
-			uint64_t reserved2 : 1;
-			uint64_t LMA : 1; // Long Mode Active
-			uint64_t NXE : 1; // No-Execute Enable
-			uint64_t SVME : 1; // Secure Virtual Machine Enable
-			uint64_t LMSLE : 1; // Long Mode Segment Limit Enable
-			uint64_t FFXSR : 1; // Fast FXSAVE/FXRSTOR
-			uint64_t TCE : 1; // Translation Cache Extension
-			uint64_t reserved3 : 1;
-			uint64_t MCOMMIT : 1; // Enable MCOMMIT instruction
-			uint64_t INTWB : 1; // Interruptible WBINVD/WBNOINVD enable
-			uint64_t reserved4 : 1;
-			uint64_t UAIE : 1; // Upper Address Ignore Enable
-			uint64_t AIBRSE : 1; // Automatic IBRS Enable
-			uint64_t reserved5 : 42;
+			uint64_t lme : 1; // Long Mode Enable
+			uint64_t reserved9 : 1;
+			uint64_t lma : 1; // Long Mode Active
+			uint64_t nxe : 1; // No-Execute Enable
+			uint64_t svme : 1; // Secure Virtual Machine Enable
+			uint64_t lmsle : 1; // Long Mode Segment Limit Enable
+			uint64_t ffxsr : 1; // Fast FXSAVE/FXRSTOR
+			uint64_t tce : 1; // Translation Cache Extension
+			uint64_t reserved16 : 1;
+			uint64_t mcommit : 1; // Enable MCOMMIT instruction
+			uint64_t intwb : 1; // Interruptible WBINVD/WBNOINVD enable
+			uint64_t reserved19 : 1;
+			uint64_t uaie : 1; // Upper Address Ignore Enable
+			uint64_t aibrse : 1; // Automatic IBRS Enable
+			uint64_t reserved22 : 42;
 		};
 		uint64_t bits;
 	};
--- VM_CR.h
+++ VM_CR.h
@@ -6,16 +6,16 @@
 
 struct VM_CR
 {
-	static constexpr uint64_t MSR_VM_CR = 0xC0010114;
+	static constexpr __int64 MSR_VM_CR = 0xC0010114;
 
 	union {
 		struct {
-			uint64_t DPD : 1; // If set, disables the external hardware debug port and certain internal debug features.
-			uint64_t R_INIT : 1; // If set, non-intercepted INIT signals are converted into an #SX exception.
-			uint64_t DIS_A20M : 1; // If set, disables A20 masking.
-			uint64_t LOCK : 1; // When this bit is set, writes to LOCK and SVMDIS are silently ignored. When this bit is clear, VM_CR bits 3 and 4 can be written. Once set, LOCK can only be cleared using the SVM_KEY MSR This bit is not affected by INIT or SKINIT.
-			uint64_t SVMDIS : 1; //When this bit is set, writes to EFER treat the SVME bit as MBZ. When this bit is clear, EFER.SVME can be written normally.This bit does not prevent CPUID from reporting that SVM is available.Setting SVMDIS while EFER.SVME is 1 generates a #GP fault, regardless of the current state of VM_CR.LOCK.This bit is not affected by SKINIT.It is cleared by INIT when LOCK is cleared to 0; otherwise, it is not affected.
-			uint64_t reserved1 : 59;
+			uint64_t dpd : 1; // If set, disables the external hardware debug port and certain internal debug features.
+			uint64_t r_init : 1; // If set, non-intercepted INIT signals are converted into an #SX exception.
+			uint64_t dis_a20m : 1; // If set, disables A20 masking.
+			uint64_t lock : 1; // When this bit is set, writes to LOCK and SVMDIS are silently ignored. When this bit is clear, VM_CR bits 3 and 4 can be written. Once set, LOCK can only be cleared using the SVM_KEY MSR This bit is not affected by INIT or SKINIT.
+			uint64_t svmdis : 1; //When this bit is set, writes to EFER treat the SVME bit as MBZ. When this bit is clear, EFER.SVME can be written normally.This bit does not prevent CPUID from reporting that SVM is available.Setting SVMDIS while EFER.SVME is 1 generates a #GP fault, regardless of the current state of VM_CR.LOCK.This bit is not affected by SKINIT.It is cleared by INIT when LOCK is cleared to 0; otherwise, it is not affected.
+			uint64_t reserved5 : 59;
 		};
 
 		uint64_t bits;
@@ -23,7 +23,7 @@ struct VM_CR
 };
 
 template<>
-void loadMSR(VM_CR& fn) 
+void loadMSR(VM_CR& fn)
 {
 	fn.bits = __readmsr(VM_CR::MSR_VM_CR);
 }
