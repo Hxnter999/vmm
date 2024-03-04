8270f7a243b1a21671094ff91d75345ada276b30
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-03-05 02:30:06 +0300

    Update commons.cpp

--- commons.cpp
+++ commons.cpp
@@ -4,6 +4,6 @@ void print(const char* fmt, ...)
 {
 	va_list args;
 	__va_start(&args, fmt);
-	vDbgPrintExWithPrefix("[VMM] ", 0, 0, fmt, args); // DPFLTR
+	vDbgPrintExWithPrefix("[VMM] ", 0, 0, fmt, args);
 	DbgPrintEx(0, 0, "\n"); // for wndbg... remove later
 }
