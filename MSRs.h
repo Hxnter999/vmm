8547fe075e98609c201109f00a8f1eda652d7a89
TDC <71852270+TDC0471@users.noreply.github.com>
2024-03-05 16:02:23 -0500

    Update MSRs.h

--- MSRs.h
+++ MSRs.h
@@ -12,16 +12,34 @@ namespace MSR {
 	void storeMSR(T& fn);
 
 	struct alignas(0x1000) msrpm {
-		// 0x0 - 0x7FF
-		util::bitset<0x800> vector1;
+		union {
+			struct {
+				// 0x0 - 0x7FF
+				util::bitset<0x800> vector1;
 
-		// 0x800 - 0xFFF
-		util::bitset<0x800> vector2;
+				// 0x800 - 0xFFF
+				util::bitset<0x800> vector2;
 
-		// 0x1000 - 0x17FF
-		util::bitset<0x800> vector3;
+				// 0x1000 - 0x17FF
+				util::bitset<0x800> vector3;
 
-		// 0x1800 - 0x1FFF
-		util::bitset<0x800> vector4;
+				// 0x1800 - 0x1FFF
+				util::bitset<0x800> vector4;
+			};
+			util::bitset<0x2000> vector;
+		};
+
+	private:
+		constexpr bool at(uint64_t MSR) const 
+		{
+			return vector.at(MSR - base);
+		}
+
+		constexpr void set(size_t index, bool value)
+		{
+			vector.set(index, value);
+		}
+
+		static constexpr uint64_t base = 0xC0000000;
 	};
 };
