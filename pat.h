1a539ad3143c2c45c36f1c74404130d6d6d22914
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-03-06 00:43:39 +0300

    page attribute table msr

---
+++ pat.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "msrs.h"
+
+namespace MSR {
+	struct PAT
+	{
+		static constexpr uint64_t MSR_PAT = 0x00000277;
+
+		union {
+			struct {
+				uint64_t pa0 : 3;
+				uint64_t reserved3 : 5;
+				uint64_t pa1 : 3;
+				uint64_t reserved11 : 5;
+				uint64_t pa2 : 3;
+				uint64_t reserved19 : 5;
+				uint64_t pa3 : 3;
+				uint64_t reserved27 : 5;
+				uint64_t pa4 : 3;
+				uint64_t reserved35 : 5;
+				uint64_t pa5 : 3;
+				uint64_t reserved43 : 5;
+				uint64_t pa6 : 3;
+				uint64_t reserved51 : 5;
+				uint64_t pa7 : 3;
+				uint64_t reserved59 : 5;
+			};
+			uint64_t bits;
+		};
+	};
+
+	template<>
+	void loadMSR(PAT& fn)
+	{
+		fn.bits = { __readmsr(PAT::MSR_PAT) };
+	}
+
+	template<>
+	void storeMSR(PAT& fn)
+	{
+		__writemsr(PAT::MSR_PAT, fn.bits);
+	}
+};
