7c00ac73bbac0b03588671d5aeee538599d8a9e7
TDC <71852270+TDC0471@users.noreply.github.com>
2024-03-05 14:48:16 -0500

    Update smart_pool.h

--- smart_pool.h
+++ smart_pool.h
@@ -1,59 +1,75 @@
-#pragma
+#pragma once
 #include "commons.h"
 
-template<typename T, size_t Tag = 0 >
-class SmartPool
+template<typename T>
+class smart_pool
 {
-	T* pool;
-	size_t size;
+    T* m_pool;
+    size_t m_size;
+
+    explicit smart_pool(T* pool, size_t size = sizeof(T)) : m_pool(pool), m_size(size) {}
 
 public:
 
-	explicit SmartPool(T* pool) : pool(pool), size(sizeof(T)) {}
+    template<typename U, POOL_TYPE pool_type = NonPagedPool>
+    friend smart_pool<U, pool_type> make_smart(size_t elements);
 
-	template<POOL_TYPE pooltype = NonPagedPool>
-	explicit SmartPool() : size(sizeof(T)) 
-	{
-		pool = static_cast<T*>(ExAllocatePoolWithTag(pooltype, size, Tag));
-	}
+    T* get() { return m_pool; }
 
-	template<size_t size, POOL_TYPE pooltype = NonPagedPool>
-	explicit SmartPool() : size(size)
-	{
-		pool = static_cast<T*>(ExAllocatePoolWithTag(pooltype, size, Tag));
-	}
-	
-	T* get() const { return pool; }
+    operator bool() { return m_pool != nullptr; }
 
-	operator bool() const { return pool != nullptr; }
+    T* operator->() { return m_pool; }
 
-	T* operator->() const { return pool; }
+    T& operator*() { return *m_pool; }
 
-	T& operator*() const { return *pool; }
+    void& operator[] (size_t index) = delete;
 
-	T operator[](size_t index) const { return pool[index]; }
+    T& operator[] (size_t index)
+    {
+        return m_pool[index];
+    }
 
-	T at(size_t index) const 
-	{
-		if(index < size)
-			return pool[index];
+    void& at(size_t index) = delete;
 
-		return T{};
-	}
-
-	SmartPool(const SmartPool&) = delete;
-	SmartPool& operator=(const SmartPool&) = delete;
-
-	SmartPool(SmartPool&& other) noexcept : pool(other.pool) { other.pool = nullptr; }
-	SmartPool& operator=(SmartPool&& other) noexcept
-	{
-		if (this != &other)
-		{
-			pool = other.pool;
-			other.pool = nullptr;
+    T& at(size_t index) 
+    {
+        if (index >= m_size / sizeof(T))
+        {
+            return T{}; // DEFAULT CONSTRUCTOR OR ZERO INIT
 		}
-		return *this;
-	}
+		return m_pool[index];
+    }
 
-	~SmartPool() { if (pool) ExFreePoolWithTag(pool, Tag); }
+    smart_pool(const smart_pool&) = delete;
+    smart_pool& operator=(const smart_pool&) = delete;
+
+    smart_pool(smart_pool&& other) noexcept : m_pool(other.m_pool) { other.m_pool = nullptr; }
+    smart_pool& operator=(smart_pool&& other) noexcept
+    {
+        if (this != &other)
+        {
+            m_pool = other.m_pool;
+            other.m_pool = nullptr;
+
+            m_size = other.m_size;
+        }
+        return *this;
+    }
+
+    ~smart_pool() {
+        if (!m_pool) return;
+        
+        for(size_t i = 0; i < m_size / sizeof(T); i++)
+        {
+			m_pool[i].~T();
+		}
+
+        ExFreePoolWithTag(m_pool, 0);
+    }
 };
+
+template<typename U, POOL_TYPE pool_type = NonPagedPool>
+inline smart_pool<U> make_smart(size_t elements = 1)
+{
+    return smart_pool<T, pool_type>(static_cast<T*>(ExAllocatePoolWithTag(pool_type, sizeof(T) * elements, 0)), sizeof(T) * elements);
+}
