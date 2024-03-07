0fa2b7b365a7c8d102149ae36bc9ac4e0a0262e0
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-03-07 19:39:44 +0300

    no more println

--- src/Source/commons.cpp
+++ src/Source/commons.cpp
@@ -5,5 +5,4 @@ void print(const char* fmt, ...)
 	va_list args;
 	__va_start(&args, fmt);
 	vDbgPrintExWithPrefix("[VMM] ", 0, 0, fmt, args);
-	DbgPrintEx(0, 0, "\n"); // for wndbg... remove later
 }
