28999b2eb7b9b504e3ea366048b56cf509d721ce
TDC <71852270+TDC0471@users.noreply.github.com>
2024-03-09 12:04:56 -0500

    Update vmcb.h

--- src/Header/ARCH/VMCB/vmcb.h
+++ src/Header/ARCH/VMCB/vmcb.h
@@ -6,12 +6,8 @@
 struct alignas(0x1000) vmcb {
 	// table b-1 (control area)
 	vmcb_control control;
-	static_assert(sizeof(vmcb_control) == 0x400, "vmcb_control size is not 0x400");
 	// table b-2 (state save area)
 	vmcb_state_save save_state;
-	static_assert(sizeof(vmcb_state_save) == 0xC00, "vmcb_state_save size is not 0xC00");
-
-	static_assert(sizeof(vmcb_control) + sizeof(vmcb_state_save) == 0x1000, "vmcb size is not 0x1000");
 };
 
 struct sharedvcpu 
