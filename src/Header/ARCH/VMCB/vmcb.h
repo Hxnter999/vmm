8f55f54e6172ff1a2afb2a209b29d12347631922
TDC <71852270+TDC0471@users.noreply.github.com>
2024-03-08 22:00:52 -0500

    remove unneeded host_state_area

--- src/Header/ARCH/VMCB/vmcb.h
+++ src/Header/ARCH/VMCB/vmcb.h
@@ -6,8 +6,12 @@
 struct alignas(0x1000) vmcb {
 	// table b-1 (control area)
 	vmcb_control control;
+	static_assert(sizeof(vmcb_control) == 0x400, "vmcb_control size is not 0x400");
 	// table b-2 (state save area)
 	vmcb_state_save save_state;
+	static_assert(sizeof(vmcb_state_save) == 0xC00, "vmcb_state_save size is not 0xC00");
+
+	static_assert(sizeof(vmcb_control) + sizeof(vmcb_state_save) == 0x1000, "vmcb size is not 0x1000");
 };
 
 struct sharedvcpu 
@@ -18,6 +22,6 @@ struct sharedvcpu
 struct alignas(0x1000) vcpu {
 	vmcb host_vmcb;
 	vmcb guest_vmcb;
-	uint8_t host_state_area[0x1000]; //Do not modfiy (depends on chipset), just set phys (page alligned) to VM_HSAVE_PA
+	//uint8_t host_state_area[0x1000]; //Do not modfiy (depends on chipset), just set phys (page alligned) to VM_HSAVE_PA
 	bool is_virtualized;
 };
