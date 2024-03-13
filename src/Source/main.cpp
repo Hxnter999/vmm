6212f89f3f07881d8d37e9fc80e68280afe76e98
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-03-13 05:03:04 +0300

    virtualized :D

--- src/Source/main.cpp
+++ src/Source/main.cpp
@@ -36,12 +36,12 @@ extern "C" {
 	extern void WHATS_A_GOOD_NAME(uint64_t* guest_vmcb_pa);
 }
 
-SVM_STATUS inittest() 
+SVM_STATUS inittest()
 {
 	CPUID::fn_vendor vendor_check{};
 	vendor_check.load();
 
-	if (!vendor_check.is_amd_vendor()) 
+	if (!vendor_check.is_amd_vendor())
 	{
 		print("Vendor check failed... get off intel nerd\n");
 		return SVM_STATUS::SVM_WRONG_VENDOR;
@@ -52,7 +52,7 @@ SVM_STATUS inittest()
 	CPUID::fn_identifiers id{};
 	id.load();
 
-	if (!id.feature_identifiers.svm) 
+	if (!id.feature_identifiers.svm)
 	{
 		print("SVM not supported, womp womp\n");
 		return SVM_STATUS::SVM_IS_NOT_SUPPORTED_BY_CPU;
@@ -67,7 +67,7 @@ SVM_STATUS inittest()
 		return SVM_STATUS::SVM_NESTED_PAGING_NOT_SUPPORTED;
 	}
 
-	if (!svm_rev.svm_feature_identification.n_rip) 
+	if (!svm_rev.svm_feature_identification.n_rip) // necessary otherwise we have to emulate it which is a pain
 	{
 		print("Uh oh! Next RIP not supported\n");
 		return SVM_STATUS::SVM_NEXT_RIP_NOT_SUPPORTED;
@@ -79,54 +79,39 @@ SVM_STATUS inittest()
 	if (!vm_cr.svmdis)
 	{
 		print("SVM not enabled but can be (;\n");
-
-
 		return SVM_STATUS::SVM_IS_CAPABLE_OF_BEING_ENABLE; // Yippe!
 	}
 
-	if (!svm_rev.svm_feature_identification.svm_lock) 
+	if (!svm_rev.svm_feature_identification.svm_lock)
 	{
 		print("SVM lock bit not set, disabled by BIOS...\n");
 		return SVM_STATUS::SVM_DISABLED_AT_BIOS_NOT_UNLOCKABLE;
 	}
-	
+
 	print("SVM lock bit set, disabled\n");
 	return SVM_STATUS::SVM_DISABLED_WITH_KEY;
 }
 
 shared global{};
-void setupvmcb(vcpu* vcpu) //dis just a test
+void setupvmcb(vcpu* vcpu, CONTEXT* ctx) //dis just a test
 {
-	global.current_vcpu = vcpu;
-
-	CONTEXT* ctx = reinterpret_cast<CONTEXT*>(ExAllocatePoolWithTag(NonPagedPool, sizeof(CONTEXT), 'sgma'));
-	memset(ctx, 0, sizeof(CONTEXT));
-	RtlCaptureContext(ctx);
-
-	if (global.current_vcpu->is_virtualized) {
-		__debugbreak();
-		print("already virtualized\n");
-		testcall();
-		testcall();
-		return;
-	}
-	global.current_vcpu->is_virtualized = true;
+	vcpu->is_virtualized = true;
 
 	print("Starting to virtualize...\n");
 	MSR::EFER efer{};
 	efer.load();
 	efer.svme = 1;
 	efer.store();
-	
+
 	vcpu->guest_vmcb.control.msrpm_base_pa = MmGetPhysicalAddress(global.shared_msrpm).QuadPart;
 
 
 	//Set up control area
 	//TODO: set interupts blah blah
 	vcpu->guest_vmcb.control.vmrun = 1; // VMRUN intercepts muse be enabled 15.5.1
-	vcpu->guest_vmcb.control.vmmcall = 1; // UM call VM
-	vcpu->guest_vmcb.control.vmmload = 1; // VMLOAD intercepts must be enabled 15.5.1
-	vcpu->guest_vmcb.control.vmmsave = 1; // VMSAVE intercepts must be enabled 15.5.1
+	vcpu->guest_vmcb.control.vmmcall = 1; // explicit vmexits back to host
+	vcpu->guest_vmcb.control.vmmload = 1;
+	vcpu->guest_vmcb.control.vmmsave = 1;
 
 	vcpu->guest_vmcb.control.guest_asid = 1; // Address space identifier "ASID [cannot be] equal to zero" 15.5.1 ASID 0 is for the host
 
@@ -141,7 +126,7 @@ void setupvmcb(vcpu* vcpu) //dis just a test
 	SEGMENT::descriptor_table_register idtr{}, gdtr{}; __sidt(&idtr); _sgdt(&gdtr);
 	vcpu->guest_vmcb.save_state.idtr.base = idtr.base;
 	vcpu->guest_vmcb.save_state.idtr.limit = idtr.limit;
-	
+
 	vcpu->guest_vmcb.save_state.gdtr.base = gdtr.base;
 	vcpu->guest_vmcb.save_state.gdtr.limit = gdtr.limit;
 
@@ -151,7 +136,7 @@ void setupvmcb(vcpu* vcpu) //dis just a test
 	vcpu->guest_vmcb.save_state.rflags = ctx->EFlags;
 
 	//vcpu->guest_vmcb.save_state.rax = ctx->Rax;
-	
+
 	//Setup all the segment registers
 	vcpu->guest_vmcb.save_state.cs.limit = __segmentlimit(ctx->SegCs);
 	vcpu->guest_vmcb.save_state.ds.limit = __segmentlimit(ctx->SegDs);
@@ -169,25 +154,27 @@ void setupvmcb(vcpu* vcpu) //dis just a test
 	vcpu->guest_vmcb.save_state.ss.get_attributes(gdtr.base);
 
 	vcpu->guest_vmcb_pa = MmGetPhysicalAddress(&vcpu->guest_vmcb).QuadPart;
+	vcpu->self = vcpu;
 
 	__svm_vmsave(MmGetPhysicalAddress(&vcpu->guest_vmcb).QuadPart);
 
 	MSR::HSAVE_PA hsave_pa{};
 	hsave_pa.bits = MmGetPhysicalAddress(&vcpu->host_state_area).QuadPart;
+	__debugbreak();
 	hsave_pa.store();
 
-	//__svm_vmsave(MmGetPhysicalAddress(&vcpu->host_vmcb).QuadPart);
+	//__svm_vmsave(MmGetPhysicalAddress(&vcpu->host_vmcb).QuadPart); idk about this
 }
 
 void Unload(PDRIVER_OBJECT pDriverObject);
 
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath) 
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
 {
 	UNREFERENCED_PARAMETER(pRegistryPath);
 
 	pDriverObject->DriverUnload = Unload;
 
-	if (!inittest()) 
+	if (!inittest())
 	{
 		print("SVM not supported\n");
 		return STATUS_UNSUCCESSFUL;
@@ -207,25 +194,27 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pR
 	}
 	memset(global.shared_msrpm, 0, sizeof(MSR::msrpm));
 
-	
+
 	for (uint32_t i = 0; i < global.vcpu_count; i++)
 	{
-		KeSetSystemAffinityThreadEx(1ll << i);
+		global.current_vcpu = &global.vcpus[i];
+		CONTEXT* ctx = reinterpret_cast<CONTEXT*>(ExAllocatePoolWithTag(NonPagedPool, sizeof(CONTEXT), 'sgma')); memset(ctx, 0, sizeof(CONTEXT));
+		RtlCaptureContext(ctx);
+
+		if (global.current_vcpu->is_virtualized) {
+			testcall();
+			continue;
+		}
+
+		auto original_affinity = KeSetSystemAffinityThreadEx(1ll << i);
 		print("attempting to set up vcpu %d\n", KeGetCurrentProcessorIndex());
 
-		setupvmcb(&global.vcpus[i]);
-		global.vcpus[i].self = &global.vcpus[i];
+		setupvmcb(&global.vcpus[i], ctx);
 
-		print("rsp: %p\n", &global.vcpus[i].guest_vmcb_pa);
-		print("stack_frame: %p\n", &global.vcpus[i].guest_stack_frame);
-		print("self: %p\n", &global.vcpus[i].self);
-		print("xmm0: %p\n", &global.vcpus[i].guest_stack_frame.xmm0);
-		print("xmm5: %p\n", &global.vcpus[i].guest_stack_frame.xmm5);
-		__debugbreak();
 		WHATS_A_GOOD_NAME(&global.vcpus[i].guest_vmcb_pa);
 
 		// this wont be executed eitherway
-		//KeRevertToUserAffinityThreadEx(original_affinity);
+		KeRevertToUserAffinityThreadEx(original_affinity);
 	}
 
 	return STATUS_SUCCESS;
