f47774590fa945e8c5ab3887d4184e4fb517d27f
TDC <71852270+TDC0471@users.noreply.github.com>
2024-03-22 17:44:53 -0400

    -if npt check

--- src/Source/Hypervisor.cpp
+++ src/Source/Hypervisor.cpp
@@ -174,9 +174,11 @@ void Hypervisor::setup_vmcb(vcpu_t* vcpu, CONTEXT* ctx)
 	vcpu->guest_vmcb.control.guest_asid = 1; // Address space identifier "ASID [cannot be] equal to zero" 15.5.1 ASID 0 is for the host
 	vcpu->guest_vmcb.control.v_intr_masking = 1; // 15.21.1 & 15.22.2
 
-	vcpu->guest_vmcb.control.np_enable = 1;
-	vcpu->guest_vmcb.control.n_cr3 = MmGetPhysicalAddress(npt).QuadPart;
-	print("NPT: %p\n", MmGetPhysicalAddress(npt).QuadPart);
+	if (npt) {
+		vcpu->guest_vmcb.control.np_enable = 1;
+		vcpu->guest_vmcb.control.n_cr3 = MmGetPhysicalAddress(npt).QuadPart;
+		print("NPT: %p\n", MmGetPhysicalAddress(npt).QuadPart);
+	}
 
 	// Set up the guest state
 	vcpu->guest_vmcb.save_state.cr0.value = __readcr0();
