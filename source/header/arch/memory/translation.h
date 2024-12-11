1d644e9033fb7f08c1617dd1a0a81a84f4ad2e4d
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2024-12-11 19:47:57 +0300

    static cast bitfields to avoid implementation defined bitfield behavior

--- source/header/arch/memory/translation.h
+++ source/header/arch/memory/translation.h
@@ -13,12 +13,12 @@ inline uint64_t gva_to_gpa(cr3_t guest_cr3, uint64_t address, uint64_t& offset_t
 	auto base = host_pt_t::mapping_base;
 	uint64_t offset{};
 
-	auto pml4e = reinterpret_cast<pml4e_t*>(base + (guest_cr3.pml4 << 12))[va.pml4_index];
+	auto pml4e = reinterpret_cast<pml4e_t*>(base + (static_cast<uint64_t>(guest_cr3.pml4) << 12))[va.pml4_index];
 	if (!pml4e.present) {
 		return 0;
 	}
 
-	auto pdpte_1gb = reinterpret_cast<pdpte_1gb_t*>(base + (pml4e.page_pa << 12))[va.pdpt_index];
+	auto pdpte_1gb = reinterpret_cast<pdpte_1gb_t*>(base + (static_cast<uint64_t>(pml4e.page_pa) << 12))[va.pdpt_index];
 	pdpte_t pdpte{ .value = pdpte_1gb.value };
 
 	if (!pdpte_1gb.present) {
@@ -27,12 +27,12 @@ inline uint64_t gva_to_gpa(cr3_t guest_cr3, uint64_t address, uint64_t& offset_t
 
 	// 1gb
 	if (pdpte_1gb.large_page) {
-		offset = (va.pd_index << (12 + 9)) + (va.pt_index << 12) + va.offset;
+		offset = (static_cast<uint64_t>(va.pd_index) << 21) + (static_cast<uint64_t>(va.pt_index) << 12) + va.offset;
 		offset_to_next_page = pdpt_address_range - offset;
-		return (pdpte_1gb.page_pa << (12 + 9 + 9)) + offset;
+		return (static_cast<uint64_t>(pdpte_1gb.page_pa) << 30) + offset;
 	}
 
-	auto pde_2mb = reinterpret_cast<pde_2mb_t*>(base + (pdpte.page_pa << 12))[va.pd_index];
+	auto pde_2mb = reinterpret_cast<pde_2mb_t*>(base + (static_cast<uint64_t>(pdpte.page_pa) << 12))[va.pd_index];
 	pde_t pde{ .value = pde_2mb.value };
 
 	if (!pde_2mb.present) {
@@ -41,20 +41,20 @@ inline uint64_t gva_to_gpa(cr3_t guest_cr3, uint64_t address, uint64_t& offset_t
 
 	// 2mb
 	if (pde_2mb.large_page) {
-		offset = (va.pt_index << 12) + va.offset;
+		offset = (static_cast<uint64_t>(va.pt_index) << 12) + va.offset;
 		offset_to_next_page = pd_address_range - offset;
-		return (pde_2mb.page_pa << (12 + 9)) + offset;
+		return (static_cast<uint64_t>(pde_2mb.page_pa) << 21) + offset;
 	}
 
 	// 4kb
-	auto pte = reinterpret_cast<pte_t*>(base + (pde.page_pa << 12))[va.pt_index];
+	auto pte = reinterpret_cast<pte_t*>(base + (static_cast<uint64_t>(pde.page_pa) << 12))[va.pt_index];
 	if (!pte.present) {
 		return 0;
 	}
 
 	offset = va.offset;
 	offset_to_next_page = pt_address_range - offset;
-	return (pte.page_pa << 12) + offset;
+	return (static_cast<uint64_t>(pte.page_pa) << 12) + offset;
 }
 
 inline uint64_t gva_to_hva(cr3_t guest_cr3, uint64_t address, uint64_t& offset_to_next_page) {
